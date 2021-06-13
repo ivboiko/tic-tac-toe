2b3bd17b8bacdc40641a264d5b3cf6c937845423
ivboiko <ivboikovst@gmail.com>
2021-06-13 20:14:18 +0300

    Redundant spacing fix

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import StartScreen from './componets/StartScreen/StartScreen';
 import {Route} from 'react-router-dom';
 
 const App = () => {
-  const [backIconShown,  setBackIconShown] = useState<boolean>(false);
+  const [backIconShown, setBackIconShown] = useState<boolean>(false);
 
   return (
     <div className="app-container">
